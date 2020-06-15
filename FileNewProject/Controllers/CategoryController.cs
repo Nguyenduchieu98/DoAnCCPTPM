75c83f6b1bf403b2d6a033520c7c37a4527c2317
Nguyenduchieu98 <49420725+Nguyenduchieu98@users.noreply.github.com>
2020-06-16 01:06:47 +0700

    Tao controllers Category

---
+++ FileNewProject/Controllers/CategoryController.cs
@@ -0,0 +1,38 @@
+﻿using FileNewProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FileNewProject.Controllers
+{
+    public class CategoryController : Controller
+    {
+        DoChoiCongNgheEntities1 db = new DoChoiCongNgheEntities1();
+        // GET: Category
+        public ActionResult Brand()
+        {
+            return PartialView(db.NhaSanXuats.ToList());
+        }
+        public ViewResult DroneBrand(int MaSX = 0)
+        {
+            //Kiểm tra chủ đề tồn tại hay không
+            NhaSanXuat cd = db.NhaSanXuats.SingleOrDefault(n => n.MaNhaSX == MaSX);
+            if (cd == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            //Truy xuất danh sách các quyển sách theo chủ đề
+            List<SanPham> lstSanPham = db.SanPhams.Where(n => n.MaNhaSX == MaSX).OrderBy(n => n.GiaBan).ToList();
+            if (lstSanPham.Count == 0)
+            {
+                ViewBag.Sach = "Không có sách nào thuộc chủ đề này";
+            }
+            //Gán danh sách chủ để
+            ViewBag.lstChuDe = db.NhaSanXuats.ToList();
+            return View(lstSanPham);
+        }
+    }
+}
