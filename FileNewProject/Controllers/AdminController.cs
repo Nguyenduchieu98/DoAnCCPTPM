50ceabcd895c6535df858670a4ce0027940ff67e
phuocloc98 <phuocloc17598@gmail.com>
2020-06-16 22:43:28 +0700

    create Admin Index View

---
+++ FileNewProject/Controllers/AdminController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FileNewProject.Models;
+using PagedList;
+using PagedList.Mvc;
+using System.IO;
+
+namespace FileNewProject.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        DoChoiCongNgheEntities1 db = new DoChoiCongNgheEntities1();
+        public ActionResult Admin(int? page)
+        {
+            //if (Session["TaiKhoan"] == null)
+            //{  
+            //    return RedirectToAction("Login", "User");
+            //}
+            int pageNumber = (page ?? 1);
+            int pageSize = 10;
+            return View(db.SanPhams.ToList().OrderBy(n => n.MaSP).ToPagedList(pageNumber, pageSize));
+        }
+
+    }
+}
