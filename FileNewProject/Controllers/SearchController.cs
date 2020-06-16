059b446c3d7b0c51b6e25b3d42a8a636e606711b
Nguyenduchieu98 <49420725+Nguyenduchieu98@users.noreply.github.com>
2020-06-16 10:07:56 +0700

    commit hieu

--- FileNewProject/Controllers/SearchController.cs
+++ FileNewProject/Controllers/SearchController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using FileNewProject.Models;
+using PagedList;
 
 namespace FileNewProject.Controllers
 {
